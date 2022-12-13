d4d1aace4f73d608b2d7ed2d5bec96ba99f7f073
Omar-Zantot <74403682+Omar-Zantot@users.noreply.github.com>
2022-12-13 21:02:53 +0200

    1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,30 @@
 ﻿
-Console.WriteLine("Online OSS section");
-Console.WriteLine("Hi");
+public abstract class Person
+{
+    public string Name;
+
+    public int Age;
+    
+    public Person (string name , int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    public abstract void print();
+} 
+public class Staff: Person { 
+
+    public double Salary;
+    public int joinYear ;
+
+    public Staff(string name, int age, double salary ,int joinYear):base(name,age)
+    {
+        Salary = salary;
+
+    }
+    public override void print()
+    {
+        Console.WriteLine($"My name is {Name} my age is {Age}+ my salary is {Salary} my joinyear is {joinYear}");
+    }
+}
+
